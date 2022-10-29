25c25511481d69330ffcd479a6d2922a25e22a94
Gean <geangs1607@gmail.com>
2022-10-28 21:08:08 -0300

    Project files

---
+++ Assets/Tests/EditMode/TesteTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TesteTest
+    {
+        [Test]
+        public void TesteTestSimplePasses()
+        {
+            Assert.AreEqual(randomscript.testInt,6);
+        }
+    }
+}
---
+++ Assets/randomscript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomscript : MonoBehaviour
+{
+
+    public static int testInt = 6;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
